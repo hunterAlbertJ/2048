307e0819b126b8488445f2055178289e3598b483
wrijlarr <wrijlarr@amazon.com>
2022-11-09 13:26:31 -0500

    Changes to tests edits

--- test/com/games/TwentyFourtyEight/Controller/GridManipulatorTest.java
+++ test/com/games/TwentyFourtyEight/Controller/GridManipulatorTest.java
@@ -2,7 +2,6 @@ package com.games.TwentyFourtyEight.Controller;
 
 import com.games.TwentyFourtyEight.controller.GridManipulator;
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 public class GridManipulatorTest {
@@ -12,17 +11,4 @@ public class GridManipulatorTest {
         GridManipulator manipulator = new GridManipulator();
         assertTrue(manipulator.isGameValid());
     }
-
-    @Test
-    public void shouldReturnBoolean_whenGameNotIsValid() {
-        GridManipulator manipulator = new GridManipulator();
-        assertEquals(true, manipulator.isGameValid());
-    }
-
-    @Test
-    public void shouldReturnNewBoard_whenBoardIsNull() {
-        GridManipulator manipulator = new GridManipulator();
-
-    }
-
 }
