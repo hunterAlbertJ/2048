947dfe4d4a0dc0d348e345f410dbfa58879f6ed6
Michael Greene <mgreene11111@yahoo.com>
2022-11-09 12:50:59 -0800

    2048 Version 1.1 - tutorial pop-up when game begins - pop up notices for win/loss conditions - tracked current score - persistent high score - writes new high score to "SaveData" file

--- src/com/games/TwentyFourtyEight/client/PlayClient.java
+++ src/com/games/TwentyFourtyEight/client/PlayClient.java
@@ -15,18 +15,21 @@ import java.awt.*;
  * Sets info for application window then starts the game.
  */
 public class PlayClient {
-    public static void main(String[] args) {
-        JLabel instructions = new JLabel(String.format("Controls - all numbered tiles slide in the direction a player moves them using the four arrow keys."
-                + "\n" + "Tiles slide as far as possible in the chosen direction until they are stopped by either another tile or the edge of the grid."
-                + "\n" + "Every turn, a new tile randomly appears in an empty spot on the board with a value of 2."
-                + "\n" + "If two tiles of the same number collide while moving, they will combine totals into a single tile."
-                + "\n" + "The resulting tile cannot merge with another tile again in the same move."
-                + "\n" + "Higher-scoring tiles change colors."
-                + "\n" + "If a move causes three consecutive tiles of the same value to slide together, only the two farthest Tiles will combine."
-                + "\n" + "If all four spaces in a row or column are filled with the same value, a move parallel to that row/column will combine the first two and last two."
-                + "\n\n" + "The game is won when a tile with a value of 2048 appears on the board."
-                + "\n\n" + "When the player has no legal moves (there are no empty spaces and no adjacent tiles with the same value), the game is over."));
+    public boolean winner = false;
 
+    public static void main(String[] args) {
+        String x = new StringBuffer().append("HOW TO PLAY")
+                .append("\n\nCONTROLS: USING THE FOUR ARROW KEYS, all numbered tiles slide in the direction a player moves them.")
+                .append("\n\n\nTiles slide as far as possible in the chosen direction until they are stopped by either another tile or the edge of the grid.")
+                .append("\nEvery turn, a new tile randomly appears in an empty spot on the board with a value of 2.")
+                .append("\nIf two tiles of the same number collide while moving, they will combine totals into a single tile.")
+                .append("\nThe resulting tile cannot merge with another tile again in the same move.")
+                .append("\nHigher-scoring tiles change colors.")
+                .append("\nIf a move causes three consecutive tiles of the same value to slide together, only the two farthest Tiles will combine.")
+                .append("\nIf all four spaces in a row or column are filled with the same value, a move parallel to that row/column will combine the first two and last two.")
+                .append("\n\nThe game is won when a tile with a value of 2048 appears on the board.")
+                .append("\n\nWhen the player has no legal moves (there are no empty spaces and no adjacent tiles with the same value), the game is over.")
+                .toString();
 
         Game game = new Game();
         JFrame window = new JFrame("Welcome to 2048");
@@ -39,5 +42,6 @@ public class PlayClient {
         window.setVisible(true);
 
         game.start();
+        JOptionPane.showMessageDialog(null, x);
     }
 }
